639623a95f354cdd3a97456fd76eebc213fab41e
Ezequiel Tamayo <EzequielTamayo@gmail.com>
2026-03-24 14:44:38 +0100

    add detail_product-view

---
+++ src/main/java/org/zeki/virtualtechseller/controller/scene/client/DetailProductViewController.java
@@ -0,0 +1,48 @@
+package org.zeki.virtualtechseller.controller.scene.client;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.VBox;
+
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class DetailProductViewController implements Initializable {
+
+    @FXML
+    private Button addCartItemBtn;
+
+    @FXML
+    private Label availableLabel;
+
+    @FXML
+    private Button buyBtn;
+
+    @FXML
+    private Label categoryLabel;
+
+    @FXML
+    private VBox descriptionBox;
+
+    @FXML
+    private Label descriptionLabel;
+
+    @FXML
+    private Label feedbackLabel;
+
+    @FXML
+    private Button gobackBtn;
+
+    @FXML
+    private Label nameLabel;
+
+    @FXML
+    private Label priceLabel;
+    
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle) {
+
+    }
+}
