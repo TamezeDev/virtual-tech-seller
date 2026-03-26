90b3609e2a6d56b2bd31c9f9b9e29dc65fa42725
Ezequiel Tamayo <EzequielTamayo@gmail.com>
2026-03-26 16:52:15 +0100

    add manual validate format pass and email fields

--- src/main/java/org/zeki/virtualtechseller/util/FormularyHelper.java
+++ src/main/java/org/zeki/virtualtechseller/util/FormularyHelper.java
@@ -24,4 +24,59 @@ public class FormularyHelper {
         }
         return false;
     }
+
+    public static boolean emailFormatValid(String email) {
+        // CHECK @
+        String[] parts = email.split("@", 2);
+        if (parts.length != 2) {
+            return false;
+        }
+        String local = parts[0];
+        String domain = parts[1];
+        // LOCAL PART CONTROL
+        if (local.isEmpty() || domain.isEmpty() || local.startsWith(".") || local.endsWith(".")
+                || local.contains("..") || !domain.contains(".")) {
+            return false;
+        }
+        String[] domainParts = domain.split("\\.", 2);
+        // DOMAIN PART CONTROL
+        if (domainParts.length != 2) {
+            return false;
+        }
+        String domainName = domainParts[0];
+        String extension = domainParts[1];
+
+        if (domainName.isEmpty() || extension.length() < 2 || domain.startsWith(".")
+                || domain.endsWith(".") || domain.contains("..")) {
+            return false;
+        }
+
+        return true;
+    }
+
+    public static boolean passFormatValid(String pass) {
+
+        boolean number = false;
+        boolean lower = false;
+        boolean upper = false;
+        boolean symbol = false;
+
+        // CHECK BOOLEANS
+        if (pass.length() >= 8) {
+            char[] parsePass = pass.toCharArray();
+            for (char ascii : parsePass) {
+                if (ascii > 64 && ascii < 91) {
+                    upper = true;
+                } else if (ascii > 96 && ascii < 123) {
+                    lower = true;
+                } else if (ascii > 47 && ascii < 58) {
+                    number = true;
+                } else if ((ascii > 32 && ascii < 48) || (ascii > 57 && ascii < 65)
+                        || (ascii > 90 && ascii < 97) || (ascii > 122 && ascii < 127)) {
+                    symbol = true;
+                }
+            }
+        }
+        return (number && lower && upper && symbol);
+    }
 }
